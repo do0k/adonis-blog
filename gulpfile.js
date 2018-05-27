ea241ee110cac568bb6092948cc82d2e6da9b0ed
do0k <do0k@outlook.com>
2018-05-28 00:44:29 +0430

    first

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+const gulp = require('gulp')
+const babel = require('gulp-babel')
+const concat = require('gulp-concat')
+const sass = require('gulp-sass')
+const uglify = require('gulp-uglify')
+const rename = require('gulp-rename')
+const sourcemaps = require('gulp-sourcemaps')
+const autoprefixer = require('gulp-autoprefixer')
+
+const paths = {
+  nodePath: 'node_modules',
+  stylesPath: 'assets/scss',
+  jsPath: 'assets/js'
+}
+
+gulp.task('js', function () {
+  return gulp.src([
+      paths.nodePath + '/jquery-slim/dist/jquery.slim.js',
+      paths.nodePath + '/bootstrap/dist/js/bootstrap.bundle.js'
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(concat('vendor-scripts.js'))
+    .pipe(gulp.dest('public'))
+    .pipe(uglify().on('error', function (err) {
+      console.log(err)
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('public'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('public'))
+})
+
+gulp.task('styles', function () {
+  return gulp.src([
+      paths.nodePath + '/bootstrap/dist/css/bootstrap.css',
+      paths.stylesPath + '/**/*.scss'
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+      })
+    )
+    .pipe(concat('styles.css'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('public'))
+})
